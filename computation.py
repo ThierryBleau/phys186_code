2925448813e5c438db7154fc9bf5631446987f67
ThierryBleau <thierryb1996@gmail.com>
2019-01-15 21:12:32 -0500

    simple algebra

---
+++ computation.py
@@ -0,0 +1,17 @@
+import math
+
+def comp1():
+    for i in range(11,33):
+        y = 1.5*i + 3
+        print(i)
+        print(y)
+
+def comp2():
+        for i in range(11,33):
+                y = 10**(-8*i+7)
+                print(i)
+                print(y)
+
+comp2()
+
+print(math.log(8 + 5.0/10,10))
